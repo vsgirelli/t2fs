7710b420f41971a586e571f9492ec8c15be6400d
vsgirelli <vavi_sg@hotmail.com>
2018-10-14 01:21:40 -0300

    Adding config file and t2fs.c with prototypes

---
+++ include/config.h
@@ -0,0 +1,17 @@
+/*
+ *  Arquivo para conter códigos de erro,
+ *  valores de prioridade,
+ *  estados das threads
+ */
+
+#ifndef __cconfig__
+#define __cconfig__
+
+// CÓDIGOS DE ERRO
+// aparentemente, ele quer valores negativos pra erro
+#define FUNC_WORKING 0
+#define FUNC_NOT_IMPLEMENTED -1
+#define FUNC_NOT_WORKING -2
+
+
+#endif
